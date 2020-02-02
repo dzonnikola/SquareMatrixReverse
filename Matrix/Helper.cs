6aaeb632f2e7181048132f43a948131609b611ab
Nikola Milovanović <nikola.milovanoviic@gmail.com>
2020-02-02 03:00:44 +0100

    Prvi commit

---
+++ Matrix/Helper.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix
+{
+    class Helper
+    {
+        public void ispisiUputstvoUvod()
+        {
+            Console.WriteLine("Izaberite nacin popunjavanja matrice\n");
+            Console.WriteLine("\tr -> random popunjavanje\n");
+            Console.WriteLine("\tm -> rucno popunjavanje\n");
+            Console.WriteLine("Vas izbor: ");
+        }
+        public void ispisiUputstvoOgledala()
+        {
+            Console.WriteLine("Unesite karakter 'r' za levo ogledalo!\n"); 
+            Console.WriteLine("Unesite karakter 'd' za donje ogledalo!\n");
+            Console.WriteLine("Unesite karakter 'b' za dijagonalu ogledala!\n");
+            Console.WriteLine("Vas izbor: ");
+        }
+
+
+        public void popuniRandomMatricu(int n, int [,] matrica)
+        {
+            Random r = new Random();
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                    matrica[i, j] = r.Next(0, 50);
+        }
+        public void popuniMatricuRucno(int n, int [,] matrica)
+        {
+            Console.WriteLine("Unesite elemente matrice!\n");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.WriteLine("matrica[{0}][{1}]: ", i + 1, j + 1); 
+                    matrica[i,j] = Convert.ToInt32(Console.ReadLine());
+                }
+                Console.WriteLine("\n"); 
+            }
+
+        }
+        public void ispisiMatricu(int n, int [,] matrica)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                    Console.Write(matrica[i,j] + " ");
+                Console.WriteLine();
+            }
+        }
+        public void desnoOgledalo(int n, int [,] matrica)
+        {
+            int tmp;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n / 2; j++)
+                {
+                    tmp = matrica[i,j];
+                    matrica[i,j] = matrica[i, n - 1 - j];
+                    matrica[i, n - 1 - j] = tmp;
+                }
+            }
+            ispisiMatricu(n, matrica);
+        }
+
+        public void donjeOgledalo(int n, int [,] matrica)
+        {
+            int tmp;
+            for (int i = 0; i < n / 2; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    tmp = matrica[i,j];
+                    matrica[i,j] = matrica[n - 1 - i,j];
+                    matrica[n - 1 - i,j] = tmp;
+                }
+            }
+            ispisiMatricu(n, matrica);
+        }
+
+        public void dijagonalnoOgledalo(int n, int [,] matrica)
+        {
+            int tmp;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n - i; j++)
+                {
+                    tmp = matrica[i,j];
+                    matrica[i,j] = matrica[n - 1 - j,n - 1 - i];
+                    matrica[n - 1 - j,n - 1 - i] = tmp;
+                }
+            }
+            ispisiMatricu(n, matrica);
+        }
+    }
+}
---
+++ Matrix/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Helper h = new Helper();
+            int n, izaberi = 0;
+            string opcijaUnosa;
+            bool popunjen = false;
+
+            Console.WriteLine("Unesite velicinu kvadratne matrice: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            int[,] matrica = new int[n,n];
+            
+            h.ispisiUputstvoUvod();
+            opcijaUnosa= Console.ReadLine();
+
+            while (!popunjen)
+            {
+                switch (opcijaUnosa)
+                {
+                    case "r":
+                        h.popuniRandomMatricu(n, matrica);
+                        h.ispisiMatricu(n, matrica);
+                        popunjen = true;
+                        break;
+                    case "m":
+                        h.popuniMatricuRucno(n, matrica);
+                        h.ispisiMatricu(n, matrica);
+                        popunjen = true;
+                        break;
+                    default:
+                        if(izaberi > 1)
+                        {
+                            Console.WriteLine("Ispunili ste broj pokusaja.");
+                            popunjen = true;
+                            break;
+                        }
+                        Console.WriteLine("Pogresan unos, pokusajte ponovo.");
+                        h.ispisiUputstvoUvod();
+                        opcijaUnosa = Console.ReadLine();
+                        izaberi++;
+                        break;
+                }
+            }
+
+            if (izaberi > 1)
+                System.Environment.Exit(0);
+
+            h.ispisiUputstvoOgledala();
+            opcijaUnosa = Console.ReadLine();
+
+            while (true)
+            {
+                switch (opcijaUnosa)
+                {
+                    case "r":
+                        h.desnoOgledalo(n, matrica);
+                        break;
+                    case "d":
+                        h.donjeOgledalo(n, matrica);
+                        break;
+                    case "b":
+                        h.dijagonalnoOgledalo(n, matrica);
+                        break;
+                    default:
+                        Console.WriteLine("Nesipravan unos!\n");
+                        System.Environment.Exit(0);
+                        break;
+                }
+                h.ispisiUputstvoOgledala();
+                opcijaUnosa = Console.ReadLine();
+            }
+        }
+    }
+}
